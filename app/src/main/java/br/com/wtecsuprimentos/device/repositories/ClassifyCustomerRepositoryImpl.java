f8c38932fa338b6e8415d03133b6436839173ac2
Daniel Henrique Lima <danielhenriquelima@gmail.com>
2021-04-13 12:17:41 -0300

    RequestBody Classification ok

--- app/src/main/java/br/com/wtecsuprimentos/device/repositories/ClassifyCustomerRepositoryImpl.java
+++ app/src/main/java/br/com/wtecsuprimentos/device/repositories/ClassifyCustomerRepositoryImpl.java
@@ -50,41 +50,61 @@ public class ClassifyCustomerRepositoryImpl implements ClassifyCustomerRepositor
     private RequestBody listToRequestBody(List<Customer> customers){
         JsonObject customersToClassify = new JsonObject();
 
-        try
-        {
-            try {
-                JsonArray outsideArray = new JsonArray();
-                JsonArray insideArray = new JsonArray();
+        try {
+            JsonArray outsideArray = new JsonArray();
+            JsonArray insideArray = new JsonArray();
 
-                Customer c = customers.get(0);
-                insideArray.add(c.getMaiorAtraso()); //OK
-                insideArray.add(c.getMargemBrutaAcumulada()); //OK
-                insideArray.add(c.getPrazoMedioRecebimentoVendas()); //OK
-                insideArray.add(c.getTitulosEmAberto()); // OK
-                insideArray.add(c.getDiferencaPercentualRisco()); //OK
-                insideArray.add(c.getPercentualRisco()); //OK
-                insideArray.add(c.getFaturamentoBruto()); //OK
-                insideArray.add(c.getMargemBruta()); //OK
-                insideArray.add(c.getPeriodoDemonstrativoEmMeses()); //OK
-                insideArray.add(c.getCustos()); //OK
-                insideArray.add(c.getAnoFundacao()); //OK
-                insideArray.add(c.getCapitalSocial()); //OK
-                insideArray.add(c.getScorePontualidade()); //OK
-                insideArray.add(1);
-                insideArray.add(0);
-                insideArray.add(0);
-                outsideArray.add(insideArray);
-                customersToClassify.add("data", outsideArray);
+            Customer c = customers.get(0);
+            insideArray.add(c.getMaiorAtraso()); //OK
+            insideArray.add(c.getMargemBrutaAcumulada()); //OK
+            insideArray.add(c.getPrazoMedioRecebimentoVendas()); //OK
+            insideArray.add(c.getTitulosEmAberto()); // OK
+            insideArray.add(c.getDiferencaPercentualRisco()); //OK
+            insideArray.add(c.getPercentualRisco()); //OK
+            insideArray.add(c.getFaturamentoBruto()); //OK
+            insideArray.add(c.getMargemBruta()); //OK
+            insideArray.add(c.getPeriodoDemonstrativoEmMeses()); //OK
+            insideArray.add(c.getCustos()); //OK
+            insideArray.add(c.getAnoFundacao()); //OK
+            insideArray.add(c.getCapitalSocial()); //OK
+            insideArray.add(c.getScorePontualidade()); //OK
 
-                String jsonString = customersToClassify.toString();
-                Log.d("DABUEK", "Resultado do json Classificação: "+jsonString);
+            switch (c.getRisco()){
+                case 4:
+                    insideArray.add(0);
+                    insideArray.add(0);
+                    insideArray.add(0);
+                    break;
 
-            }catch(Exception e){
+                case 3:
+                    insideArray.add(0);
+                    insideArray.add(1);
+                    insideArray.add(0);
+                    break;
 
+                case 2:
+                    insideArray.add(1);
+                    insideArray.add(0);
+                    insideArray.add(0);
+                    break;
+
+                case 1:
+                    insideArray.add(0);
+                    insideArray.add(0);
+                    insideArray.add(1);
+                    break;
             }
-        }catch (Exception e){
+
+            outsideArray.add(insideArray);
+            customersToClassify.add("data", outsideArray);
+
+            String jsonString = customersToClassify.toString();
+            Log.d("DABUEK", "Resultado do json Classificação: "+jsonString);
+
+        }catch(Exception e){
 
         }
+
         RequestBody rBody = RequestBody.create(MediaType.parse("application/json"), customersToClassify.toString());
         return rBody;
     }
